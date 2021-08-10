8a40866b2d7be5c22bc8e3f847738df7c952ff11
zc <2@qq.com>
2021-08-10 21:04:00 +0800

    first commit

---
+++ mqproductservice/src/main/java/com/zc/MqproductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqproductserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqproductserviceApplication.class, args);
+    }
+
+}
---
+++ mqproductservice/src/test/java/com/zc/mqproductservice/MqproductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.mqproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqproductserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
